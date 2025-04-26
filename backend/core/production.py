699a2baa44b88bbf10f95baa84dcf4279e4be7c2
shaibin <shaibin16@gmail.com>
2025-04-26 20:51:36 +0530

    Fifteen

--- backend/core/production.py
+++ backend/core/production.py
@@ -62,7 +62,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'filevault'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 } 
